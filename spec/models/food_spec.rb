81f3a15983e6b366b311d1f375ad5676ac86c5bb
Muhammad Syukur Abadi <64145699+sykrabadi@users.noreply.github.com>
2022-04-14 10:25:01 +0000

    Initial commit

---
+++ spec/models/food_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+  it 'is valid with a name and a description' do
+    food = Food.new(
+      name: 'Nasi Uduk',
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 15000.0
+    )
+
+    expect(food).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    food = Food.new(
+      name: nil,
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 15000.0
+    )
+
+    food.valid?
+
+    expect(food.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate name" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+    
+    food2 = Food.new(
+      name: "Nasi Uduk",
+      description: "Just with a different description.",
+      price: 10000.0
+    )
+
+    food2.valid?
+    
+    expect(food2.errors[:name]).to include("has already been taken")
+  end
+end
