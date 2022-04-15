204e7e9dc64223e5c1db8feb6f112e7f5f0a4f60
Muhammad Syukur Abadi <64145699+sykrabadi@users.noreply.github.com>
2022-04-15 03:35:11 +0000

    add numericality spec on food's price column

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,5 @@
 class Food < ApplicationRecord
   belongs_to :category
   validates :name, presence: true, uniqueness: true
+  validates :price, numericality: {greater_than: 0.01}
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -48,7 +48,29 @@ RSpec.describe Food, type: :model do
     expect(food2.errors[:name]).to include("has already been taken")
   end
 
-  it "return food that has at least a category" do
-    
+  it "should check if the price is greater than 0.01" do
+    category = Category.new(name: "Lunch")
+    food = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 0.0001,
+      category: category
+    )
+
+    food.valid?
+    expect(food.errors[:price]).to include ("must be greater than 0.01")
+  end
+
+  it "should check if the price only accept numeric" do
+    category = Category.new(name: "Lunch")
+    food = Food.new(
+      name: 'Nasi Uduk',
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: "2000s",
+      category: category
+    )
+
+    food.valid?
+    expect(food.errors[:price]).to include("is not a number")
   end
 end
