fa503f3057bc80c58656b6afab4a341e1475a11d
Muhammad Syukur Abadi <64145699+sykrabadi@users.noreply.github.com>
2022-04-15 02:46:53 +0000

    add validation spec on category column

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "valid with name" do
+    category = Category.new(
+      name: "Beverage"
+    )
+
+    expect(category).to be_valid
+  end
+
+  it "invalid without name" do
+    category = Category.new(
+      name: nil
+    )
+
+    category.valid?
+
+    expect(category.errors[:name]).to include ("can't be blank")
+  end
 end
