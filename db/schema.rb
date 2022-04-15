e9eb4af964a8e1b0e5b3479192720cbdb4b36e6f
Muhammad Syukur Abadi <64145699+sykrabadi@users.noreply.github.com>
2022-04-15 14:21:01 +0000

    create customer table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_15_030523) do
+ActiveRecord::Schema.define(version: 2022_04_15_142006) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "emai"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/customers.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :customer do
+    name { "MyString" }
+    emai { "MyString" }
+  end
+end
